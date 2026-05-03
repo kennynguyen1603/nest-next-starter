6f77c2cfc6df9adc32ff53be64e5b13b233ef29b
Ny Nguyễn <126080786+kennynguyen1603@users.noreply.github.com>
2026-05-03 20:03:21 +0700

    feat(users): add users CRUD module with dual-persistence (relational/document)

---
+++ apps/api/src/users/users.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { DocumentUserPersistenceModule } from './infrastructure/persistence/document/document-persistence.module';
+import { RelationalUserPersistenceModule } from './infrastructure/persistence/relational/relational-persistence.module';
+
+import { FilesModule } from '@/files/files.module';
+import databaseConfig from '@/config/database/database.config';
+import { DatabaseConfig } from '@/config/database/database-config.type';
+
+// <database-block>
+const infrastructurePersistenceModule = (databaseConfig() as DatabaseConfig)
+  .isDocumentDatabase
+  ? DocumentUserPersistenceModule
+  : RelationalUserPersistenceModule;
+// </database-block>
+
+@Module({
+  imports: [infrastructurePersistenceModule, FilesModule],
+  controllers: [UsersController],
+  providers: [UsersService],
+  exports: [UsersService, infrastructurePersistenceModule],
+})
+export class UsersModule {}
