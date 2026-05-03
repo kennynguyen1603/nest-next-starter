e49bdabec18fc079684895e97b883bd07b49a621
Ny Nguyễn <126080786+kennynguyen1603@users.noreply.github.com>
2026-05-03 20:03:11 +0700

    feat(bootstrap): add application entry point and root module

---
+++ apps/api/src/app.module.ts
@@ -0,0 +1,84 @@
+import path from 'path';
+
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { HeaderResolver, I18nModule } from 'nestjs-i18n';
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+import { AllConfigType } from './config/config.type';
+import databaseConfig from './config/database/database.config';
+import { DatabaseConfig } from './config/database/database-config.type';
+import { MongooseConfigService } from './database/mongoose-config.service';
+import { TypeOrmConfigService } from './database/typeorm-config.service';
+import authConfig from './config/auth/auth.config';
+import appConfig from './config/app/app.config';
+import mailConfig from './config/mail/mail.config';
+
+import { UsersModule } from './users/users.module';
+import { FilesModule } from './files/files.module';
+import { AuthModule } from './auth/auth.module';
+import { SessionModule } from './session/session.module';
+import { MailModule } from './mail/mail.module';
+import { MailerModule } from './mailer/mailer.module';
+
+// <database-block>
+const infrastructureDatabaseModule = (databaseConfig() as DatabaseConfig)
+  .isDocumentDatabase
+  ? MongooseModule.forRootAsync({
+      useClass: MongooseConfigService,
+    })
+  : TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
+      dataSourceFactory: async (options?: DataSourceOptions) => {
+        if (!options) {
+          throw new Error('Invalid options passed to DataSource');
+        }
+        return new DataSource(options).initialize();
+      },
+    });
+// </database-block>
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig, authConfig, appConfig, mailConfig],
+      envFilePath: ['.env'],
+    }),
+    infrastructureDatabaseModule,
+    I18nModule.forRootAsync({
+      useFactory: (configService: ConfigService<AllConfigType>) => ({
+        fallbackLanguage: configService.getOrThrow('app.fallbackLanguage', {
+          infer: true,
+        }),
+        loaderOptions: { path: path.join(__dirname, '/i18n/'), watch: true },
+      }),
+      resolvers: [
+        {
+          use: HeaderResolver,
+          useFactory: (
+            configService: ConfigService<AllConfigType>,
+          ): string[] => {
+            return [
+              configService.getOrThrow<string>('app.headerLanguage', {
+                infer: true,
+              }),
+            ];
+          },
+          inject: [ConfigService],
+        },
+      ],
+      imports: [ConfigModule],
+      inject: [ConfigService],
+    }),
+    UsersModule,
+    FilesModule,
+    AuthModule,
+    SessionModule,
+    MailModule,
+    MailerModule,
+  ],
+})
+export class AppModule {}
