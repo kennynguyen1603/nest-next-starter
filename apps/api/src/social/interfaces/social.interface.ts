a4cb0f93fb1a952b080b6df5b5f992e2327f25c8
Ny Nguyễn <126080786+kennynguyen1603@users.noreply.github.com>
2026-05-04 21:38:36 +0700

    chore(social): add SocialProviders type and provider field to interface

--- apps/api/src/social/interfaces/social.interface.ts
+++ apps/api/src/social/interfaces/social.interface.ts
@@ -1,6 +1,9 @@
+export type SocialProviders = 'google' | 'github' | 'twitter';
+
 export interface SocialInterface {
   id: string;
   firstName?: string;
   lastName?: string;
   email?: string;
+  provider: SocialProviders;
 }
